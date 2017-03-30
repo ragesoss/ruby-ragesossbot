d3fd536bde102b4cb0c23bf30eb5065657989227
Sage Ross <ragesoss@gmail.com>
2017-03-30 15:04:49 -0700

    Start building sql query for new users
    Also copy wiki.rb from FixmeBot

---
+++ labs_database.rb
@@ -0,0 +1,18 @@
+require 'mysql2'
+require 'yaml'
+
+class LabsDatabase
+  def initialize
+    config = YAML.load File.read('database.yml')
+    @client = Mysql2::Client.new(config)
+  end
+
+  def new_users_with_edits(max_edit_count: 5)
+    @client.query(
+      "SELECT * FROM user
+      WHERE user_editcount < #{max_edit_count}
+      ORDER BY user_id DESC
+      LIMIT 100"
+    )
+  end
+end
