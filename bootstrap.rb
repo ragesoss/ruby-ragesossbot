1cb4f7e4b24f4da5ea87aec18d838ec0440b49d6
Sage Ross <ragesoss@gmail.com>
2017-03-30 15:46:16 -0700

    Gem name vs require name?

--- bootstrap.rb
+++ bootstrap.rb
@@ -1,7 +1,7 @@
 # Run these commands to get started from scratch
 
 require 'sqlite3'
-require 'activerecord'
+require 'active_record'
 
 SQLite3::Database.new('ragesossbot.sqlite3')
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'ragesossbot.sqlite3')
--- user.rb
+++ user.rb
@@ -1,4 +1,4 @@
-require 'activerecord'
+require 'active_record'
 require 'sqlite3'
 require 'logger'
 
